95ae6a928615b0cd7dd563c8642c446c2aed2d97
Pablouc <lpablouc9@gmail.com>
2023-06-08 16:25:18 -0600

    arduino

--- arduino/spi_slave/spi_slave.ino
+++ arduino/spi_slave/spi_slave.ino
@@ -1,18 +1,15 @@
+
 #include "pins_arduino.h"
 #include <Servo.h>
 
-Servo motor_x, motor_y, motor_z;
-
 volatile byte operation;
 volatile boolean process_it;
+Servo motor_x, motor_y, motor_z;
+int counter = 0;
 
+bool shouldStop = false;
 
-
-void setup (void)
-{
-  Serial.begin (9600);   // debugging
-
-  // have to send on master in, *slave out*
+void setup() {
   pinMode(MISO, OUTPUT);
   
   // turn on SPI in slave mode
@@ -23,15 +20,12 @@ void setup (void)
   
   operation = 0;
   process_it = false;
-
   motor_x.attach(3);
   motor_y.attach(4);
   motor_z.attach(5);
-  
-}  // end of setup
+  Serial.begin(9600);
+}
 
-
-// SPI interrupt routine
 ISR (SPI_STC_vect)
 {
   operation = SPDR;
@@ -39,10 +33,49 @@ ISR (SPI_STC_vect)
   process_it = true;
 }
 
-// main loop - wait for flag set in interrupt routine
-void loop (void)
-{
-  delay(1000);
+void pick_four(int multiplier){
+  motor_x.write(55);
+  delay(2000);
+  motor_z.write(60 * multiplier);
+  delay(2000);
+  motor_z.write(0);
+  delay(2000);
+  motor_x.write(0);
+  delay(2000);
+}
+
+
+void pick_five(int multiplier){
+  motor_x.write(30);
+  delay(2000);
+  motor_z.write(40 * multiplier);
+  delay(2000);
+  motor_z.write(0);
+  delay(2000);
+  motor_x.write(0);
+  delay(2000);
+  
+}
+
+
+void pick_six(int multiplier){
+  //motor_y.write(20);
+  //delay(2000);
+  motor_z.write(40);
+  delay(2000);
+  motor_y.write(0);
+  delay(2000);
+  motor_z.write(0);
+  delay(2000);
+}
+
+
+void loop() {
+  motor_x.write(0);
+  motor_y.write(0);
+  motor_z.write(0);
+  delay(2000);
+  
   int data_to_respond = 10;
   if(process_it) {
     switch (operation) {
@@ -60,34 +93,15 @@ void loop (void)
         break;
       case 4:
         Serial.println("Press 4");
-          motor_x.write(55);
-          delay(2000);
-          motor_z.write(60);
-          delay(2000);
-          motor_z.write(0);
-          delay(2000);
-          motor_x.write(0);
-          delay(2000);
+        pick_four(1);
         break;
       case 5:
         Serial.println("Press 5");
-        motor_x.write(30);
-        delay(2000);
-        motor_z.write(40);
-        delay(2000);
-        motor_z.write(0);
-        delay(2000);
-        motor_x.write(0);
-        delay(2000);
+        pick_five(1);
         break;
       case 6:
         Serial.println("Press 6");
-        motor_z.write(40);
-        delay(2000);
-        motor_y.write(0);
-        delay(2000);
-        motor_z.write(0);
-        delay(2000);
+        pick_six(1);
         break;
       case 7:
         Serial.println("Press 7");
@@ -116,4 +130,6 @@ void loop (void)
     process_it = false;
     SPDR = data_to_respond;
   }
-}  // end of loop
+ 
+
+}
